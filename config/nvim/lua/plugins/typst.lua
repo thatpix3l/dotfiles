30ba7948f61e80b96ca5a561fd863a51bbc1e16d
thatpix3l <git@thatpix3l.com>
2023-09-28 04:08:31 +0000

    feat(typst): add PDF viewer for typst files

--- config/nvim/lua/plugins/typst.lua
+++ config/nvim/lua/plugins/typst.lua
@@ -11,10 +11,22 @@ return {
         }
     },
     config = function()
+        -- Default PDF viewer
+        vim.g.typst_pdf_viewer = "mupdf"
+
+        -- Recompile typ file when typing
         require("lspconfig").typst_lsp.setup({
             settings = {
                 exportPdf = "onType"
             }
+        })
+
+        -- Refresh PDF viewer on text changes
+        vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+            pattern = { "*.typ" },
+            callback = function()
+                vim.fn.jobstart("pkill --signal HUP mupdf")
+            end
         })
     end
 }
